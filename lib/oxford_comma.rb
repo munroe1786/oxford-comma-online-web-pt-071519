cff110ea6ed6b88e81c153eb9aed8838db2e2914
Eliza Munroe <era3munroe@gmail.com>
2019-07-24 12:09:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if 
+    array.count == 2
+    array.join(" and ")
+  elsif 
+  array.count > 2
+  last_element = array.pop
+  array.push("and")
+  array.join(", ")
+  array.push(last_element)
+  else 
+    array.join
+end
 end
