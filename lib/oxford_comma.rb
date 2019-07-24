a2a624a75cac25b53cd88d4937147cd84c18c85b
Eliza Munroe <era3munroe@gmail.com>
2019-07-24 13:05:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if 
+    array.count == 2
+    array.join(" and ")
+  elsif 
+    array.count > 2
+    array[-1].prepend ("and ")
+    array.join(", ")
+  else 
+    array.join
+end
 end
